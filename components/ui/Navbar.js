cbabc4058729e404ab19bad923b301c8d6a8969e
maticamisay <maticamisay99@hotmail.com>
2023-09-30 02:42:15 -0300

    fix navbar style and title

--- components/ui/Navbar.js
+++ components/ui/Navbar.js
@@ -4,12 +4,12 @@ const Navbar = () => {
   return (
     <nav className="bg-blue-500 p-4 mt-0 w-full">
       <div className="container mx-auto flex flex-wrap items-center">
-        <div className="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
+        <div className="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold text-2xl">
           <a
             href="#"
             className="text-white no-underline hover:text-white hover:no-underline"
           >
-            🌟 Mi Proyecto con Graphql
+            🌟 TO DO&apos;s con GraphQL
           </a>
         </div>
       </div>
